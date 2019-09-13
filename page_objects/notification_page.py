d3911102df8c202479bbcb9d66afe3f2bf975039
ors <orsikik@gmail.com>
2019-09-13 19:02:25 +0300

    Page objects created

---
+++ page_objects/notification_page.py
@@ -0,0 +1,11 @@
+from base_page import BasePage
+from base_element import BaseElement
+from selenium.webdriver.common.by import By
+
+
+class NotificationPage(BasePage):
+    url = 'https://my.ausweis.io/ru/notifications/list/'
+
+    def lock_last_notification(self):
+        return BaseElement(self.driver, by=By.XPATH, value="//tbody/tr[1]/td")
+
